63815b571b1bffe2e51fb21ecf22ad03922f913a
Faisal-khan12 <sahirfls@gmail.com>
2022-07-21 17:46:52 +0500

    new commit by Fk 21/7/2022

--- app/src/main/java/com/appstacks/indiannaukribazaar/NewActivities/AddJobsActivity.java
+++ app/src/main/java/com/appstacks/indiannaukribazaar/NewActivities/AddJobsActivity.java
@@ -23,7 +23,6 @@ public class AddJobsActivity extends AppCompatActivity {
 
     ActivityAddJobsBinding binding;
 
-    RadioGroup rg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +31,77 @@ public class AddJobsActivity extends AppCompatActivity {
         binding = ActivityAddJobsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        rg =findViewById(R.id.radio_group);
 
         Intent i = getIntent();
         String mcg = i.getStringExtra("mcg");
         binding.txtPositon.setText(mcg);
+
+        iconChange();
+
+
+        binding.jobPosition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), JobPosition.class));
+            }
+        });
+
+
+        binding.btnWorkplaceType.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                BottomSheetDialog dialog = new BottomSheetDialog(AddJobsActivity.this, R.style.AppBottomSheetDialogTheme);
+
+                View bottomsheetView = LayoutInflater.from(getApplicationContext()).
+                        inflate(R.layout.bottom_sheet_workplace, (ConstraintLayout) findViewById(R.id.bottom_sheet_container));
+
+                dialog.setContentView(bottomsheetView);
+                dialog.show();
+
+                RadioButton onsite = bottomsheetView.findViewById(R.id.onSitebtn);
+                RadioButton hybrid = bottomsheetView.findViewById(R.id.hybridBtn);
+                RadioButton remote = bottomsheetView.findViewById(R.id.remoteBtn);
+                onsite.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String onSitTxt = onsite.getText().toString();
+                        binding.txtWorkplace.setText(onSitTxt);
+                        binding.btnAdd2.setVisibility(View.INVISIBLE);
+                        binding.btnEdit2.setVisibility(View.VISIBLE);
+
+
+                    }
+                });
+
+                hybrid.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        String hybridTxt = hybrid.getText().toString();
+                        binding.txtWorkplace.setText(hybridTxt);
+
+                    }
+                });
+
+                remote.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String remoteTxt = remote.getText().toString();
+                        binding.txtWorkplace.setText(remoteTxt);
+                        dialog.dismiss();
+                    }
+                });
+
+
+            }
+        });
+
+
+    }
+
+
+    private void iconChange() {
 
 
         if (binding.txtPositon.length() != 0) {
@@ -48,56 +113,6 @@ public class AddJobsActivity extends AppCompatActivity {
         } else {
             binding.btnAdd1.setVisibility(View.VISIBLE);
             binding.btnEdit1.setVisibility(View.INVISIBLE);
-
-
         }
-
-
-        binding.jobPosition.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), JobPosition.class));
-            }
-        });
-        binding.btnWorkplaceType.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                BottomSheetDialog dialog = new BottomSheetDialog(AddJobsActivity.this,R.style.AppBottomSheetDialogTheme);
-
-                View bottomsheetView = LayoutInflater.from(getApplicationContext()).
-                        inflate(R.layout.bottom_sheet_workplace,(ConstraintLayout)findViewById(R.id.bottom_sheet_container));
-
-                dialog.setContentView(bottomsheetView);
-                dialog.show();
-
-
-
-//                bottomsheetView.findViewById(R.id.onSitebtn).setOnClickListener(new View.OnClickListener() {dw
-//                    @Override
-//                    public void onClick(View view) {
-//
-//                        bottomsheetView.setOnClickListener(new View.OnClickListener() {
-//                            @Override
-//                            public void onClick(View view) {
-//
-//
-//
-//                            }
-//                        });
-//                    }
-//                });
-                
-            }
-        });
-
-
-
-
-
-
-
-
-
     }
 }
