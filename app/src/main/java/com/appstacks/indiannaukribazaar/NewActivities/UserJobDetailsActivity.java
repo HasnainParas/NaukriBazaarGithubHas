cc36db24e5e762f5434c1508e5489c0c5427a676
Hasnain <hasnainahmed576@gmail.com>
2022-12-14 18:24:12 +0500

    work in jobPostingDetails by Hasnain

---
+++ app/src/main/java/com/appstacks/indiannaukribazaar/NewActivities/UserJobDetailsActivity.java
@@ -0,0 +1,19 @@
+package com.appstacks.indiannaukribazaar.NewActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.appstacks.indiannaukribazaar.R;
+
+public class UserJobDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_job_details);
+
+
+
+    }
+}
